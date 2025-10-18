cc04d69797fd655805460c16d46862cf63a015c2
Kevin Jin <1912247288@qq.com>
2025-10-18 16:19:24 +0800

    quartus project

---
+++ quartus_prj/ip_core/PLL25MHz/pll_25mhz_inst.v
@@ -0,0 +1,6 @@
+pll_25mhz	pll_25mhz_inst (
+	.areset ( areset_sig ),
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig ),
+	.locked ( locked_sig )
+	);
---
+++ quartus_prj/ip_core/fifo_256x8/fifo_256x8_inst.v
@@ -0,0 +1,9 @@
+fifo_256x8	fifo_256x8_inst (
+	.aclr ( aclr_sig ),
+	.clock ( clock_sig ),
+	.data ( data_sig ),
+	.rdreq ( rdreq_sig ),
+	.wrreq ( wrreq_sig ),
+	.full ( full_sig ),
+	.q ( q_sig )
+	);
